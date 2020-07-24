76c19b9fdbabe4eb5b0b96ffd37df9f14cd85e21
tomgao <406038564@qq.com>
2020-07-24 16:34:49 +0800

    deploy

---
+++ precache-manifest.1f7cc7d3b052f34248087009e608bcb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9648002d94ae1029594a002a27aaa4",
+    "url": "/xiaobei-react-website/index.html"
+  },
+  {
+    "revision": "83864444083c2f8e35cf",
+    "url": "/xiaobei-react-website/static/css/main.4f2ca4ef.chunk.css"
+  },
+  {
+    "revision": "07ecb0d294d94575514c",
+    "url": "/xiaobei-react-website/static/js/2.83129982.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/xiaobei-react-website/static/js/2.83129982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83864444083c2f8e35cf",
+    "url": "/xiaobei-react-website/static/js/main.b56f0e2b.chunk.js"
+  },
+  {
+    "revision": "0e49ecbb5363b7ec3301",
+    "url": "/xiaobei-react-website/static/js/runtime-main.9ceffa77.js"
+  }
+]);
